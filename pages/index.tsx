3076ea1e6699717cd850b84ff9ace2d008f74feb
jonathan <joni7840850@gmail.com>
2023-08-11 11:14:59 -0600

    Add more elements to home page

---
+++ pages/index.tsx
@@ -0,0 +1,45 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../componets/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link';
+import Date from '../componets/date';
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Physics Graduate with an emphasis on optimization and computational physics. 
+          Enthusiastic about artificial intelligence, backend development, and computing. 
+          I possess experience in analyzing real-world problems and resolving them 
+          within the academic sphere through research and computational simulation.</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
